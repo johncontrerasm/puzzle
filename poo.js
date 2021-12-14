a016c5c99d9b5327994aba0df7948effc64e48d6
John Contreras <john.contreras@aveco.com>
2021-12-13 19:55:12 -0500

    mas cambios

--- poo.js
+++ poo.js
@@ -1,124 +1,5 @@
 const _private = new WeakMap();
 
-class Board {
-
-    constructor(movementsCounter) {
-        const properties = {
-            _movementsCounter: movementsCounter,
-            _instructions: "* El objetivo es colocar los números 1,2,3 en la primera fila, 4,5,6 en la segunda y 7,8 en la tercera.\n "
-                + "* La celda vacía debe quedar en la esquina inferior derecha. \n"
-                + "* Al presionar un botón con numero si la celda presionada, tiene una celda vacía vecina, esta celda ocupará la celda vacía. \n"
-                + "* Si se presiona una celda sin tener la celda vacía de vecina, se contará como un movimiento.",
-
-
-        }
-
-        _private.set(this, { properties })
-    }
-
-    /*
-            Getter and setters
-    */
-    get movementsCounter() {
-        return _private.get(this).properties['_movementsCounter'];
-    }
-
-    set movementsCounter(movement) {
-        return _private.get(this).properties['_movementsCounter'] = movement;
-    }
-
-    get instructions() {
-        return _private.get(this).properties['_instructions'];
-    }
-
-    /*
-            Metodos funcionales
-    */
-
-    calculateMovementsCounter() {
-        console.log("Muchos movimientos");
-    }
-
-    isGridSorted() {
-        console.log("False");
-    }
-
-
-
-    myleft(miposicion) {
-
-        if (miPosicion == 0 || miPosicion == 3 || miPosicion == 6)
-            return -1;
-        else
-            return miPosicion - 1;
-
-    }
-
-
-    myRight(miposicion) {
-        if (miPosicion == 2 || miPosicion == 5 || miPosicion == 8)
-            return -1;
-        else
-            return miPosicion + 1;
-    }
-
-
-    inMyUp(miposicion) {
-        if (miPosicion == 0 || miPosicion == 1 || miPosicion == 2)
-            return -1;
-        else
-            return miPosicion - 3;
-    }
-
-    inMyDown(miposicion) {
-        if (miPosicion == 6 || miPosicion == 7 || miPosicion == 8)
-            return miPosicion - 1;
-        else
-            return miPosicion + 3
-    }
-
-
-    isSwapable(squares, posicion) {
-        var pos;
-        var VacioEs = -1;
-        pos = this.myleft(posicion);
-        if (pos !== -1)
-            if (squares[pos].imEmpty)
-                VacioEs = pos
-        if (VacioEs == -1)
-            pos = this.myRight(posicion);
-            if (pos !== -1)
-                if (squares[pos].imEmpty)
-                    VacioEs = pos   
-        if (VacioEs == -1)
-            pos = this.inMyUp(posicion);
-            if (pos !== -1)
-                if (squares[pos].imEmpty)
-                    VacioEs = pos
-        if (VacioEs == -1)
-            pos = this.inMyUp(posicion);
-            if (pos !== -1)
-                if (squares[pos].imEmpty)
-                    VacioEs = pos                    
-    }
-
-
-    swap(posicion,posicionvacia,squares){
-        var squareemp;
-        squaretemp=squares[posicion];
-        squares[posicion]=squares[posicionvacia];
-        squares[posicionvacia]=squaretemp;
-        var uno = document.getElementById('b' + posicion);
-        uno.innerHTML = squares[posicion].ID;
-        var uno = document.getElementById('b' + posicionvacia);
-        uno.innerHTML = squares[posicionvacia].ID;
-    }
-    
-
-
-}
-
-
 class Square {
 
     constructor(ID, color) {
@@ -151,20 +32,164 @@ class Square {
     }
 
 
-
-    /*             get isSwapable(){
-                    return _private.get(this).properties['_isSwapable'];
-                }
-            
-                set isSwapable(state){
-                    return _private.get(this).properties['_isSwapable'] = state;
-                } */
-
-
     imEmpty() {
-        return ID == "";
+        //return ID == "";
+        return ID;
     }
 }
+
+
+class Board {
+
+    constructor(movementsCounter,squares) {
+
+            this._movementsCounter= movementsCounter,
+            this._instructions= "* El objetivo es colocar los números 1,2,3 en la primera fila, 4,5,6 en la segunda y 7,8 en la tercera.\n "
+                + "* La celda vacía debe quedar en la esquina inferior derecha. \n"
+                + "* Al presionar un botón con numero si la celda presionada, tiene una celda vacía vecina, esta celda ocupará la celda vacía. \n"
+                + "* Si se presiona una celda sin tener la celda vacía de vecina, se contará como un movimiento.",
+            this._squares=squares
+
+    }
+
+    /*
+            Getter and setters
+    */
+    movementsCounter() {
+        return this._movementsCounter;
+    }
+
+    setmovementsCounter(movement) {
+        return this._movementsCounter = movement;
+    }
+
+    instructions() {
+        return this._instructions;
+    }
+
+    /*
+            Metodos funcionales
+    */
+
+    calculateMovementsCounter() {
+        this._movementsCounter++;
+        console.log("Muchos movimientos"+this._movementsCounter);
+    }
+
+    isGridSorted() {
+        console.log("False");
+    }
+
+
+
+    myleft(miPosicion) {
+        miPosicion=parseInt(miPosicion)
+        if (miPosicion == 0 || miPosicion == 3 || miPosicion == 6)
+            return -1;
+        else
+            return miPosicion - 1;
+
+    }
+
+
+    myRight(miPosicion) {
+        miPosicion=parseInt(miPosicion)
+        if (miPosicion == 2 || miPosicion == 5 || miPosicion == 8)
+            return -1;
+        else
+            return miPosicion + 1;
+    }
+
+
+    inMyUp(miPosicion) {
+        miPosicion=parseInt(miPosicion)
+        if (miPosicion == 0 || miPosicion == 1 || miPosicion == 2)
+            return -1;
+        else
+            return miPosicion - 3;
+    }
+
+    inMyDown(miPosicion) {
+        miPosicion=parseInt(miPosicion)
+        if (miPosicion == 6 || miPosicion == 7 || miPosicion == 8)
+            return -1;
+        else
+            return miPosicion + 3
+    }
+
+
+    isSwapable(posicion) {
+        var pos;
+        var VacioEs = -1;
+        pos = this.myleft(posicion);
+        if (pos !== -1)
+            if (this._squares[pos].ID == "")
+                VacioEs = pos
+        if (VacioEs == -1)
+            pos = this.myRight(posicion);
+            if (pos !== -1)
+                if (this._squares[pos].imEmpty == "")
+                    VacioEs = pos   
+        if (VacioEs == -1)
+            pos = this.inMyUp(posicion);
+            if (pos !== -1)
+                if (this._squares[pos].imEmpty == "")
+                    VacioEs = pos
+        if (VacioEs == -1)
+            pos = this.inMyUp(posicion);
+            if (pos !== -1)
+                if (this._squares[pos].imEmpty == "")
+                    VacioEs = pos
+        return VacioEs                    
+    }
+
+
+    swap(posicion,posicionvacia){
+
+        console.log("Posicion "+ posicion)
+        console.log("Posicion Vacia "+ posicionvacia)
+        var uno = document.getElementById('b' + posicion);
+        console.log("TEST "+ this._squares[posicion].ID)
+        uno.innerHTML = this._squares[posicion].ID;
+        var uno = document.getElementById('b' + posicionvacia);
+        uno.innerHTML = this._squares[posicionvacia].ID;
+        
+
+
+        var posicion1=parseInt(posicion);   
+        var squaretemp=this._squares[posicion1];
+        this._squares[posicion1]=this._squares[posicionvacia];
+        this._squares[posicionvacia]=squaretemp;
+        
+    }
+
+    restartManual() {
+
+        var lista = [1, 2, 3, 4, 5, 6, 7, 8, ""];
+        lista = lista.sort(function () { return Math.random() - 0.5 });
+        console.log(lista)
+        for (let i = 0; i < lista.length; i++) {
+            this._squares[i] = new Square(lista[i], "Rojo");
+            var uno = document.getElementById('b' + i);
+            uno.innerHTML = lista[i];
+        }
+    }
+
+    workOnClick(posicion){
+        console.log("workonclic"+ this._squares)
+        let vacio=this.isSwapable(posicion)
+        console.log("Vacio " +vacio)
+        if (vacio != -1){
+            this.swap(posicion,vacio);
+        }
+    }
+    
+
+
+}
+
+
+
 
 /************************ */
 //********* MAIN ***********
@@ -173,38 +198,33 @@ class Square {
 
 
 //Objeto Board
+var cuadrado = [];
+const Board1 = new Board(0,cuadrado);
+Board1.restartManual();
 
-const Board1 = new Board(0);
-restartManual();
 
-
-
-function restartManual() {
-
-    console.log("Reiniciando el tablero");
-    var lista = [1, 2, 3, 4, 5, 6, 7, 8, ""];
-    let squares = []
-    lista = lista.sort(function () { return Math.random() - 0.5 });
-    console.log(lista)
-    for (let i = 0; i < lista.length; i++) {
-        squares[i] = new Square(lista[i], "Rojo");
-        var uno = document.getElementById('b' + i);
-        uno.innerHTML = lista[i];
-    }
-}
-
-let matriz = [[1, 2, 3], [4, 5, 6], [7, 8, ""]];
-console.log(matriz[0][1])
-console.log(matriz)
 
 
 function processClick(evento) {
     // console.log(evento.innerHTML)
     console.log(evento.id.charAt(1)) //posicion boton
+    console.log(Board1.inMyDown(evento.id.charAt(1)));  
+    posicion=evento.id.charAt(1);
+    Board1.calculateMovementsCounter();
+    Board1.workOnClick(parseInt(posicion));
+/*     console.log("CUADRADOS"+squares);
+    console.log("VALOR"+squares[posicion].ID)
+    console.log("IZQ"+Board1.myleft(posicion));
+    console.log("DER"+Board1.myRight(posicion));
+    console.log("UP"+Board1.inMyUp(posicion));
+    console.log("DOWN"+Board1.inMyDown(posicion)); */
+
 }
 
 function showInstructions() {
     alert(Board1.instructions);
 }
 
-
+function restartManualDOM(){
+    Board1.restartManual();
+}
